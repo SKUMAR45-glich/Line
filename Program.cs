a189ec6ca9c55038edad801c42b8565b044ea892
ABC <abc@gmail.com>
2020-09-27 03:21:08 +0530

    uc3

--- Program.cs
+++ Program.cs
@@ -11,7 +11,19 @@ namespace uc1
             int y1 = 3;
             int y2 = 4;
             double d1 = Math.Sqrt(Math.Pow(x2 - x1, 2) + Math.Pow(y2 - y1, 2));
-            Console.WriteLine(d1);
+            int x11 = 2;
+            int x22 = 3;
+            int y11 = 4;
+            int y22 = 5;
+            double d2 = Math.Sqrt(Math.Pow(x22 - x11, 2) + Math.Pow(y22 - y11, 2));
+            if (d1 == d2)
+            {
+                Console.WriteLine("Equals");
+            }
+            else
+            {
+                Console.WriteLine("Not Equal");
+            }
         }
     }
 }
