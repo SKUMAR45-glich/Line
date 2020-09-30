4ea2a7cfed02f82b436489636d80d1b675819822
SKUMAR45-glich <71806111+SKUMAR45-glich@users.noreply.github.com>
2020-09-30 09:17:20 +0530

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -6,15 +6,15 @@ namespace uc1
     {
         static void Main(string[] args)
         {
-            int x1 = 1;
-            int x2 = 2;
-            int y1 = 3;
-            int y2 = 4;
+            int x1 = Convert.ToInt32(Console.ReadLine());
+            int x2 = Convert.ToInt32(Console.ReadLine());
+            int y1 = Convert.ToInt32(Console.ReadLine());
+            int y2 = Convert.ToInt32(Console.ReadLine());
             double d1 = Math.Sqrt(Math.Pow(x2 - x1, 2) + Math.Pow(y2 - y1, 2));
-            int x11 = 2;
-            int x22 = 3;
-            int y11 = 4;
-            int y22 = 5;
+            int x11 = Convert.ToInt32(Console.ReadLine());
+            int x22 = Convert.ToInt32(Console.ReadLine());
+            int y11 = Convert.ToInt32(Console.ReadLine());
+            int y22 = Convert.ToInt32(Console.ReadLine());
             double d2 = Math.Sqrt(Math.Pow(x22 - x11, 2) + Math.Pow(y22 - y11, 2));
             if (d1==d2)
             {
